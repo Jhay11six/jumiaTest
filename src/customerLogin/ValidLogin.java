0072d9d8d5baf68c4282a7c1b0aef8ac186d3db1
Jhay11six <olaminiyibadero@gmail.com>
2019-11-05 15:26:36 +0100

    firstcomment

---
+++ src/customerLogin/ValidLogin.java
@@ -0,0 +1,32 @@
+package customerLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ValidLogin {
+	
+	public static void main(String[] args) {
+		
+		//to set the property  iof the test
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Iykedagenius\\Desktop\\summit\\chromedriver_win32\\chromedriver.exe");
+		
+		//to instance of driver
+		WebDriver driver = new ChromeDriver();
+		
+		//WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://www.jumia.com.ng/");
+		
+		driver.findElement(By.xpath(" //label[contains(text(),'Login')]")).click();
+		
+		driver.findElement(By.xpath("//a[@class='btn _prim -fw _md']")).click();
+		
+		
+	}
+
+}
